576459af4b5245b40965e2ebd0582663dae2afbd
Aimi Ross <touyamaaimi922@gmail.com>
2019-11-06 18:22:42 -0800

    Initial Commit

---
+++ upDate1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  upDate1
+//
+//  Created by AIMI ROSS on 11/6/19.
+//  Copyright © 2019 AIMI ROSS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
